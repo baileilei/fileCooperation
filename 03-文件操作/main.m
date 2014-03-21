4a54e5121ddf9a71c580c33107defce304b68c4d
apple <aplle@xiaozhe.local>
2014-03-21 15:16:17 +0800

    Initial Commit

---
+++ 03-文件操作/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  03-文件操作
+//
+//  Created by apple on 14-3-21.
+//  Copyright (c) 2014年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
